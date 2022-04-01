e20a363864df26e539248f30680c74af125b7875
Kongduino <dbvic@me.com>
2022-04-01 10:09:22 +0800

    Added Hash

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 
 An RUI3 port of the Adafruit nRFCrypto library, and more specifically of [my fork](https://github.com/Kongduino/Adafruit_nRFCrypto). Since RUI3 is a bare metal platform, without RTOS, the library cannot work as is, so I had to hack things apart, and put them back together. So far I have implemented `nRFCrypto_Random`, an existing class from the library, and `nRFCrypto_AES`, my own addition to the library, which provides hardware-accelerated AES encryption and decryption, ECB, CBC and CTR.
 
-The sample code demonstrates AES encryption for nRF52840 devices (here specifically RAK4631 using RUI3), while using `nRFCrypto_Random` for the key and IV. I will add `nRFCrypto_Hash` to the example, to round it up with HMAC.
+The sample code demonstrates AES encryption for nRF52840 devices (here specifically RAK4631 using RUI3), while using `nRFCrypto_Random` for the key and IV. I have also added `nRFCrypto_Hash` to the example, using the example code from Adafruit.
 
+TODO: round it up with HMAC.
 
 
 ![Screenshot.png](Screenshot_A.png)
 ![Screenshot.png](Screenshot_B.png)
+![Screenshot.png](Screenshot_C.png)
--- RUI3_nRFCrypto_AES.ino
+++ RUI3_nRFCrypto_AES.ino
@@ -1,9 +1,11 @@
 #include <cstring>
 #include "nRF_AES.h"
 #include "nRF_Random.h"
+#include "nRF_Hash.h"
 
 nRFCrypto_AES aes;
 nRFCrypto_Random rnd;
+nRFCrypto_Hash myHash;
 
 void hexDump(unsigned char *buf, uint16_t len) {
   char alphabet[17] = "0123456789abcdef";
@@ -30,6 +32,37 @@ void hexDump(unsigned char *buf, uint16_t len) {
     Serial.print(s);
   }
   Serial.print(F("   +------------------------------------------------+ +----------------+\n"));
+}
+
+/* Input data for testing
+  Same data is stored in input_data.bin file, to verify the result, run hash sum on your PC
+  and compare the result with this sketch
+  $ sha1sum input_data.bin
+  9f9da10ec23735930089a8f89b34f7b5d267903e
+  $ sha224sum input_data.bin
+  68abe34d09a758be6b2fb3a7a997983a639687099d35406f927a5cc5
+  $ sha256sum input_data.bin
+  75cfb39b62c474921e2aad979c210f8b69180a9d58e9f296a4b9904ae6e7aa40
+  $ sha512sum input_data.bin
+  e3979c6296e282af04619992f71addfefd118be26626cedd715edced36b87058f868b316e725b24e1e7f661ce2935e44ba4deea62afa3e13188071403a2f1463
+*/
+uint8_t input_data[] = {
+  0x6b, 0xc1, 0xbe, 0xe2, 0x2e, 0x40, 0x9f, 0x96, 0xe9, 0x3d, 0x7e, 0x11, 0x73, 0x93, 0x17, 0x2a,
+  0x65, 0xa2, 0x32, 0xd6, 0xbc, 0xd0, 0xf9, 0x39, 0xed, 0x1f, 0xe1, 0x28, 0xc1, 0x3b, 0x0e, 0x1b
+};
+
+void test_hash(uint32_t mode, const char* modestr) {
+  uint32_t result[16];
+  uint8_t result_len; // depending on Hash mode
+  myHash.begin(mode);
+  myHash.update(input_data, sizeof(input_data));
+  result_len = myHash.end(result);
+  Serial.print(" ");
+  Serial.flush();
+  Serial.println(modestr);
+  hexDump((unsigned char *) result, result_len);
+  Serial.println();
+  Serial.flush();
 }
 
 void setup() {
@@ -102,6 +135,14 @@ void loop() {
   rslt = aes.Process(encBuf, rslt, IV, pKey, pKeyLen, decBuf, aes.decryptFlag, aes.ctrMode);
   Serial.println("CTR Decoded:");
   hexDump((unsigned char *)decBuf, rslt);
+
+  test_hash(CRYS_HASH_SHA1_mode, "SHA-1");
+  test_hash(CRYS_HASH_SHA224_mode, "SHA-224");
+  test_hash(CRYS_HASH_SHA256_mode, "SHA-256");
+  test_hash(CRYS_HASH_SHA512_mode, "SHA-512");
+  // Note: SHA384 and MD5 currently cause hardfault
+  // test_hash(CRYS_HASH_SHA384_mode, "SHA384");
+  // test_hash(CRYS_HASH_MD5_mode, "MD5");
 
   delay(10000);
 }
